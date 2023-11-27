04ba86c1a3a43a2569926e5d1438e7946a60bea5
Rystambek <rustambekjumaqulov@gmail.com>
2023-11-27 21:56:17 +0500

    as_view model

--- app/urls.py
+++ app/urls.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import (
+from app.views.car import CarView
+from app.views.customer import CustomerView
+from app.views.sum import (
             home,
             index,
             get_num,
             get_sum,
             get_user,
-            car,
         )
 
 urlpatterns = [
-    path('car/<int:id>',car),
-    path('car/',car),
-    path('',index),
-    path('home/',home),
+    path('car/<int:id>',CarView.as_view()),
+    path('car/',CarView.as_view()),
+    path('customer/',CustomerView.as_view()),
+    path('customer/<int:id>',CustomerView.as_view()),
+    path('',home),
+    path('index/',index),
     path('get-num/',get_num),
     path('get-sum/',get_sum),
     path('get-user/<str:username>',get_user)
